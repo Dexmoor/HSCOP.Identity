55559a29dd4dfa9744dd3e587fad41cbda34d5f1
John Churchley <john.churchley@nhs.net>
2023-10-30 22:20:50 +0000

    HSCOP-ID Initial upload create project and basic get JWT

---
+++ HSCOP.Identity.Service/AuthenticateService.cs
@@ -0,0 +1,41 @@
+﻿using HSCOP.Identity.Service.Options;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace HSCOP.Identity.Service
+{
+    public class AuthenticateService : IAuthenticateService
+    {
+        private readonly JWTSettings _jwtSettings;
+
+        public AuthenticateService(IOptionsMonitor<JWTSettings> jwtSettingsOptions) {
+            _jwtSettings = jwtSettingsOptions.CurrentValue;
+        }
+
+        public string GetJWTToken()
+        {
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "Admin"),
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.ClientAppId,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromDays(7)),
+                signingCredentials: credentials,
+                claims: claims
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return tokenString;
+        }
+    }
+}
---
+++ HSCOP.IdentityApi/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using HSCOP.Identity.Service;
+using HSCOP.Identity.Service.Options;
+using Microsoft.AspNetCore.Authentication;
+
+namespace HSCOP.Identity.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterInjectedServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            SetConfigurationOptions(services, configuration);
+            ConfigureScopedServices(services);
+        }
+
+        private static void SetConfigurationOptions(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JWTSettings>(configuration.GetSection(nameof(JWTSettings)));
+        }
+
+        private static void ConfigureScopedServices(IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticateService, AuthenticateService>();
+        }
+    }
+}
